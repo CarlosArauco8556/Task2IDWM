729d5c33a1f486ac06e26659a69263dd16a11831
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-01 18:42:40 -0300

    feat: routes configuration.

--- src/app/Porfolio/Pages/main-page/main-page.component.ts
+++ src/app/Porfolio/Pages/main-page/main-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../Components/navbar/navbar.component';
+import { HomeComponent } from '../../Components/home/home.component';
+import { DescriptionComponent } from '../../Components/description/description.component';
+import { SkillsComponent } from '../../Components/skills/skills.component';
+import { ProjectsComponent } from '../../Components/projects/projects.component';
+import { ContactFormComponent } from '../../Components/contact-form/contact-form.component';
 
 @Component({
   selector: 'main-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, HomeComponent, DescriptionComponent, SkillsComponent, ProjectsComponent, ContactFormComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'porfolio',
+        pathMatch: 'full',
+        loadComponent: () => import('./Porfolio/Pages/main-page/main-page.component').then(m => m.MainPageComponent)
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'portfolio'
+    }
+];
