7914bff799476c53292a53e9b96a58991d04e548
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-01 23:59:46 -0300

    feat: fixed error and add function in navbar.

--- src/app/Porfolio/Components/navbar/navbar.component.ts
+++ src/app/Porfolio/Components/navbar/navbar.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   //Variable to store the active link.
-  activeLink: string = 'home';
+  activeSection: string = '';
+  scrolled: boolean = false;
 
-  //Function to activate the link.
-  setActive(link: string): void {
-    this.activeLink = link;
+  // Function to change the active link when the user scrolls.
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    this.scrolled = scrollTop > 50; // Change the value to the desired scroll value.
+  }
+  //Function to scroll to the section.
+  scrollToSection(sectionId: string): void {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      this.activeSection = sectionId; 
+    }
   }
   
-  //Function to verify if the link is active.
+  //Function to verify if the section is active.
   isActive(link: string): boolean {
-    return this.activeLink === link;
+    return this.activeSection === link;
   }
 }
