f084d9f80a9b815db8390ad677f6a2a932256d79
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-02 03:03:57 -0300

    feat: contact-form finished.

--- src/app/Porfolio/Components/contact-form/contact-form.component.ts
+++ src/app/Porfolio/Components/contact-form/contact-form.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'contact-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
+  contactForm!: FormGroup;
+  isSubmitting = false;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      email: [''],
+      message: ['']
+    });
+  }
+
+  onSubmit(): void {
+    this.isSubmitting = true;
+
+    setTimeout(() => {
+      this.isSubmitting = false;
+      this.showConfirmationModal();
+    }, 2000); // 2 segundos de espera
+  }
+
+  showConfirmationModal(): void {
+    const modal = document.getElementById('confirmationModal');
+    if (modal) {
+      modal.classList.remove('hidden');
+    }
+  }
+
+  closeModal(): void {
+    const modal = document.getElementById('confirmationModal');
+    if (modal) {
+      modal.classList.add('hidden');
+    }
+
+    this.contactForm.reset();
+  }
 }
